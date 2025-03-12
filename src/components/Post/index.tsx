f2d9e209a5887eb3729af8996962df8f77944025
NeptunesOrca <orcabot002@gmail.com>
2025-03-12 16:56:22 -0600

    Added the interaction buttons to posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,10 +1,11 @@
 //import { faUser } from '@fortawesome/free-regular-svg-icons';
-/*import {
-  faHouse,
-  faPeopleGroup,
-  faShop,
+import {
+  faHeart,
+  faComment,
+  faTurnDown,
+  faBookmark
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';*/
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { memo } from 'react';
 
 //import { useNavigate } from 'react-router';
@@ -21,13 +22,24 @@ const Post = (props: PostProps) => {
   //let navigate = useNavigate();
 
   return (
-    <div className={styles.post}>
-      <div className = {styles.postHeader}>
-        <img src={props.userimgsrc} width="10%" height= "10%" alt="" border-radius="50"></img>
-        <div className = {styles.postUser}><h2>{props.username}</h2></div>
+    <div className ={styles.outerBox}>
+      <div className={styles.post}>
+        <div className = {styles.postHeader}>
+          <img src={props.userimgsrc} width="10%" height= "10%" alt="" border-radius="50"></img>
+          <div className = {styles.postUser}><h2>{props.username}</h2></div>
+        </div>
+        <div className = {styles.postContents}>
+          {props.title}
+        </div>
       </div>
-      <div className = {styles.postContents}>
-        {props.title}
+      <div className={styles.buttonIcons}>
+        <div className= {styles.actionButton}><FontAwesomeIcon icon={faBookmark} size="1x" /></div>
+        <div className = {styles.actionButton}>
+          <div className= {styles.mirrored}><FontAwesomeIcon icon={faComment} size = "1x"/></div>
+        </div>
+        <div className = {styles.actionButton}>
+          <div className = {styles.rotate90}><FontAwesomeIcon icon={faTurnDown} size="1x" /></div>
+        </div>
       </div>
     </div>
   );
